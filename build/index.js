006a13fe4b71495a1c217fe1126a7187362c5a45
Geksanit <31949379+Geksanit@users.noreply.github.com>
2017-10-13 15:28:28 +0700

    add calendar

--- build/index.js
+++ build/index.js
@@ -235,21 +235,29 @@ var weekDay = function () {
     n = n ? n-1 : 6;
     return n
 };
+var date = new Date();
+var today = date.getDate();
+var month = date.getMonth();
+var year = date.getFullYear();
 
-var calendarChange = function(element){
-    var date = new Date();
-    var today = date.getDate();
-    element.children[0].innerHTML = today;
-    var month = date.getMonth();
-    element.children[1].childNodes[0].data = monthNames[month];
-    var calendar = new Date();
+var calendarChange = function(element,year,month){
+    var calendar = new Date(year, month);
     calendar.weekDay = weekDay;
+    //console.log(calendar.getFullYear(), calendar.getMonth(),calendar.getDate());
+    month = calendar.getMonth();
+    year = calendar.getFullYear();
 
-    calendar.setDate(1);
+    element.children[0].innerHTML = today;
+    element.children[1].childNodes[0].data = monthNames[month];
+    element.children[2].innerHTML = year;
+
+    var table = document.createElement('tbody');
+    table.setAttribute('data-month', month);
+    table.setAttribute('data-year', calendar.getFullYear());
+
     calendar.setDate(1-calendar.weekDay());//последний понедельник предыдущего месяца
-    var table = element.children[3].children[0];
 
-    while(calendar.getMonth() <= month){//заполнение таблицы
+    for(var j=0; j<6; j++){//заполнение таблицы
         var tr = document.createElement('tr');
         for(var i=0; i<7; i++){//неделя
 
@@ -257,22 +265,43 @@ var calendarChange = function(element){
             td.innerHTML = calendar.getDate();
             if(calendar.getMonth() == month){
                 td.className = "calendar__days-month";};
-            if(calendar.getMonth() == month && calendar.getDate() == today){
+            if(calendar.getDate() == today &&
+                calendar.getMonth() == date.getMonth() &&
+                calendar.getFullYear() == date.getFullYear()){
                 td.className = "calendar__today";};
             calendar.setDate(calendar.getDate() + 1);
             tr.appendChild(td);
         };
         table.appendChild(tr);
       };
+    element.children[4].replaceChild(table, element.children[4].children[0]);
 };
 
 var calendarInit = function (){
      var elements = document.querySelectorAll('.calendar');
      for(var i=0; i<elements.length; i++){
-         calendarChange(elements[i]);
+         calendarChange(elements[i],year,month);//календарь текущего месяца
      }
  }();
 
+document.left = function(event){
+    //console.log('clocked left');
+    var element = event.target.parentElement.parentElement;
+    var year = Number(element.children[4].children[0].getAttribute('data-year'));
+    var month = Number(element.children[4].children[0].getAttribute('data-month'));
+    calendarChange(element,year,month-1);
+};
+document.right = function (event) {
+    //console.log('clicked right');
+    var element = event.target.parentElement.parentElement;
+    var year = Number(element.children[4].children[0].getAttribute('data-year'));
+    var month = Number(element.children[4].children[0].getAttribute('data-month'));
+    calendarChange(element,year,month+1);
+};
+document.today = function(event){
+    var element = event.target.parentElement;
+    calendarChange(element,year,month);
+};
 
 /***/ })
 /******/ ]);
