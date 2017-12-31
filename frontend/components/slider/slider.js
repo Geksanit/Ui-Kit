3ae2dfe5235656bac7cd2a33724d7b3f4bb3850b
Geksanit <31949379+Geksanit@users.noreply.github.com>
2017-12-31 19:35:55 +0700

    Обьединил слайдеры (#10)

--- frontend/components/slider/slider.js
+++ frontend/components/slider/slider.js
@@ -1,11 +1,20 @@
 // slider
+
 const sliderChange = function sliderChange(element) {
   const { value } = element;
-  const width = element.parentElement.clientWidth - 20;
+  const parent = element.parentElement;
+  const width = parent.clientWidth - 20;
   const min = element.attributes.min.value;
   const max = element.attributes.max.value;
-  element.previousElementSibling.innerText = value;
-  element.previousElementSibling.style.left = `${((width / (max - min)) * (value - min)) - 8.75}px`;
+  const view = parent.querySelector('.slider__view');
+  if (view) {
+    view.innerText = value;
+    view.style.left = `${((width / (max - min)) * (value - min)) - 8.75}px`;
+  }
+  const line = parent.querySelector('.slider__line');
+  if (line) {
+    line.style.width = `${((width / (max - min)) * (value - min)) + 1}px`;
+  }
 };
 
 const sliderInput = function sliderInput(event) {
